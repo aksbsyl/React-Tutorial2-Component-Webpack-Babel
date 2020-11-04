728d8d3da7e52c2183ddc1711b8367f3a5f5c8fe
Aakash Bashyal <50483193+aksbsyl@users.noreply.github.com>
2020-11-05 01:40:16 +0545

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    //render method is used to render or export output
+    return <h1> Hello from App</h1>;
+  }
+}
+//To use our class somewhere else we use export
+export default App;
---
+++ index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// //component in React
+
+// function App() {
+//   return <h1> Hello React</h1>;
+// }
+
+ReactDOM.render(<App />, document.getElementById("root"));
+//render is method that comes from ReactDOM. We can pass two parameters here
+//first is component(functin based component), second the location where we want to keep our data
+
+//We make components in separate files
